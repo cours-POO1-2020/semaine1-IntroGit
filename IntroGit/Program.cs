59b2519f7d6e451de09cc6ceb3ad5f89b264d957
julbrunet <59811048+julbrunet@users.noreply.github.com>
2020-08-27 10:39:41 -0400

    Add introGit sample

---
+++ IntroGit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Semaine1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour mes chers,");
+            Console.WriteLine("Vous avez brillament cloné votre premier projet!");
+
+            Console.ReadKey();
+
+            Console.WriteLine("Et si vous essayiez de rajouter votre propre message?");
+            Console.ReadKey();
+
+        }
+    }
+}
